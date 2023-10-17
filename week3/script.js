09aca01e9570abb618a094556bfe63a95740b5de
Mohamed Hamza Ghazi <mg2049@hw.ac.uk>
2023-10-17 01:03:40 -0800

    form validation done

--- week3/script.js
+++ week3/script.js
@@ -40,3 +40,63 @@ function validateUsername() {
   }
 }
 
+function validateEmail() {
+  const emailValue = email.value.trim();
+  const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (emailValue === "") {
+    emailError.textContent = "Email cannot be empty.";
+    email.classList.remove("success");
+    email.classList.add("error");
+    return false;
+  } else if (!pattern.test(emailValue)) {
+    emailError.textContent = "Invalid email format.";
+    email.classList.remove("success");
+    email.classList.add("error");
+    return false;
+  } else {
+    emailError.textContent = "";
+    email.classList.remove("error");
+    email.classList.add("success");
+    return true;
+  }
+}
+
+function validatePassword() {
+  const passwordValue = password.value.trim();
+  if (passwordValue === "") {
+    passwordError.textContent = "Password cannot be empty.";
+    password.classList.remove("success");
+    password.classList.add("error");
+    return false;
+  } else if (passwordValue.length < 8) {
+    passwordError.textContent = "Must be 8 characters in length";
+    password.classList.remove("success");
+    password.classList.add("error");
+    return false;
+  } else {
+    passwordError.textContent = "";
+    password.classList.remove("error");
+    password.classList.add("success");
+    return true;
+  }
+}
+
+function validateConfirmPassword() {
+  const confirmPasswordValue = confirmPassword.value.trim();
+  if (confirmPasswordValue === "") {
+    confirmPasswordError.textContent = "Confirm Password cannot be empty.";
+    confirmPassword.classList.remove("success");
+    confirmPassword.classList.add("error");
+    return false;
+  } else if (confirmPasswordValue !== password.value.trim()) {
+    confirmPasswordError.textContent = "Passwords Do Not Match !!";
+    confirmPassword.classList.remove("success");
+    confirmPassword.classList.add("error");
+    return false;
+  } else {
+    confirmPasswordError.textContent = "";
+    confirmPassword.classList.remove("error");
+    confirmPassword.classList.add("success");
+    return true;
+  }
+}
