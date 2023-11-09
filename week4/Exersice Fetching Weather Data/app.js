3a516944c67a268ac4dcdec08b45f3992975b756
Mohamed Hamza Ghazi <mg2049@hw.ac.uk>
2023-11-09 05:13:51 -0800

    updated

--- week4/Exersice Fetching Weather Data/app.js
+++ week4/Exersice Fetching Weather Data/app.js
@@ -1,18 +1,18 @@
-// Step 1:Variable declaration to store reffrences 
+// Variable declaration to store reffrences 
 const userCity = document.getElementById("cityInput"); 
 const weather_info = document.getElementById("weather-info"); 
 const btn = document.getElementById("btn"); 
 
-// Step 2: adding an event listner to button
+// adding an event listner to button
 btn.addEventListener("click", () => {
-    // Step 3: getting the city name 
+    // getting the city name 
     const name_City = userCity.value;
 
-    // Step 4:error checking to see if the field is left blank 
+    // error checking to see if the field is left blank 
     if (name_City === "") {
         alert("Please enter a city name.");
     } else {
-        // Step 5: making a request using our api key 
+        //  making a request using our api key 
         const key_Api = '4b0e3d64123a349d711d464c1ba50f99';
         const Api_Url = `https://api.openweathermap.org/data/2.5/weather?q=${name_City}&appid=${key_Api}`;
 
